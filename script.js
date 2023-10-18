a1a768fed2cbc9c0568aa5ddeceab1a6ed2738ed
Suraj kumar <86525589+Suraj07kumar@users.noreply.github.com>
2023-10-18 14:37:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+console.log("connected");
+
+setTimeout(()=>{
+    h1.innerHTML = "Anonymus (‿ˠ‿)"
+},1000)
+
+let a1 = () => setTimeout(() => {
+    s1.innerHTML = "Initializing Hacking...";
+}, 2000)
+
+let a2 = ()=> setTimeout(() => {
+    s2.innerHTML = "Hacking Gaurav's IPhone...";
+}, 3000)
+
+let a3 = ()=> setTimeout(() => {
+    s3.innerHTML = "    username found : <br>' kgisnarutofan@gmail.com '";
+}, 5000)
+
+let a4 = ()=> setTimeout(() => {
+    s4.innerHTML = "Retriving Data...";
+}, 6000)
+
+let a5 = ()=> setTimeout(() => {
+    s5.innerHTML = "Hacked! <br> Hey Gaurav, let's Play a game💀";
+}, 9000)
+
+
+a1();
+a2();
+a3();
+a4();
+a5();
